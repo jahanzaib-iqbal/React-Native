ef1770ecd77b8f5611d466ab02424a0713ad4dd6
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2023-10-24 03:33:35 +0500

    Update FirstScreen.js

--- Mids Practice/FirstScreen.js
+++ Mids Practice/FirstScreen.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { View, Text, Button, StyleSheet, useState } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import { View, Text, Button, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const FirstScreen = () => {
   const navigation = useNavigation();
-
   const [count, setCount] = React.useState(1);
+  const [bg, setbg] = useState(global.setting.bg);
+  const [fc, setfc] = useState(global.setting.fc);
+  
+
+  useEffect(() => {
+    // console.log("Count Mounted");
+    console.log(" AT startup bg : ", bg, "fc : ", fc);
+
+  }, [count]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      console.log("FirstScreen Focused");
+      // console.log(`in 1st COlor bg : ${global.setting.bg} fc : ${global.setting.fc}df`);
+      setbg(global.setting.bg);
+      setfc(global.setting.fc);
+
+      console.log("bg : ", bg, "fc : ", fc);
+    });
+    return unsubscribe;
+  }, []);
 
   const addCount = () => {
-    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1);
   };
 
-
   return (
-    <View style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: bg,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Text>This is my FirstScreen</Text>
       <Text style={{ fontSize: 40 }}> Count : {count}</Text>
       <Button title="Add +1" onPress={addCount} />
@@ -28,12 +55,3 @@ const FirstScreen = () => {
 };
 
 export default FirstScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
