4448ce50d5db7b7b820cc92b216d547da538e381
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2023-10-24 00:03:07 +0500

    Update SecondScreen.js

--- Mids Practice/SecondScreen.js
+++ Mids Practice/SecondScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native"
+
 const SecondScreen = () => {
-    const navigation = useNavigation();
+
+  const route = useRoute();
+    const navigation = useNavigation({navigation, route});
+   const {dummyData} = route.params;
   return (
     <View>
       <Text>This is my SecondScreen</Text>
+      <Text>Data from Previous Screen : {dummyData}</Text>
       <Button title="Goto Back" onPress={() => navigation.goBack()} />
     </View>
   );
