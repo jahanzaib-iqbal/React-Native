238e6a39947b1e3dcf985a2684a9adcda9c99eed
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2023-10-24 00:03:43 +0500

    Practiced Use States

--- Mids Practice/FirstScreen.js
+++ Mids Practice/FirstScreen.js
@@ -1,21 +1,39 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import SecondScreen from "./SecondScreen";
-// import { NavigationContainer } from "@react-navigation/native";
+import { View, Text, Button, StyleSheet, useState } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const FirstScreen = () => {
   const navigation = useNavigation();
 
+  const [count, setCount] = React.useState(1);
+
+  const addCount = () => {
+    setCount((prevCount) => prevCount + 1)
+  };
+
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>This is my FirstScreen</Text>
+      <Text style={{ fontSize: 40 }}> Count : {count}</Text>
+      <Button title="Add +1" onPress={addCount} />
       <Button
         title="Goto Next"
-        onPress={() => navigation.navigate("SecondScreen")}
+        onPress={() =>
+          navigation.navigate("SecondScreen", { dummyData: "Hello World" })
+        }
       />
     </View>
   );
 };
 
 export default FirstScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
